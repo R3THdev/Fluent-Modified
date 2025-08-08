a6c559da2ff501b29778650b8af474f4302c7c2b
R3THdev <notr3th@gmail.com>
2025-08-08 20:19:46 +0800

    Update InterfaceManager.lua

--- Addons/InterfaceManager.lua
+++ Addons/InterfaceManager.lua
@@ -1,6 +1,14 @@
 local HttpService = game:GetService("HttpService")
 
 local InterfaceManager = {} do
+    InterfaceManager.Folder = "FluentSettings"
+    InterfaceManager.Settings = {
+        Theme = "Darker",
+        Acrylic = false,
+        Transparency = false,
+        MenuKeybind = "LeftControl"
+    }
+
     function InterfaceManager:SetFolder(folder)
         self.Folder = folder
         self:BuildFolderTree()
